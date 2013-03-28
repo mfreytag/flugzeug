b9e6f3b74c3a4651d61488fd29be4d1f57a7083d
mfreytag <mfreytag@gmail.com>
2013-03-28 11:05:29 +0100

    added menu

---
+++ menu.java
@@ -0,0 +1,45 @@
+/*
+
+*/
+public class menu
+{
+  String f0[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f1[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f2[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f3[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f4[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f5[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f6[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f7[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  String f8[] = {" "," "," "," "," "," "," "," "," "," "," "," "};
+  
+  
+  public static void main(String[] args)
+  {
+    System.out.println("t0         08 - 09 - 10 - 11 - 12 - 13 - 14 - 15 - 16 - 17 - 18 - 19 - 20 | Auslastung | Einnahmen\n");
+    
+    System.out.printf("flugzeug0     x    x    x    x    x    x    x                                58,3  %%     7 x 130 EUR%n");
+    
+    System.out.printf("flugzeug1                              x    x    x    x                      33,3  %%     4 x 130 EUR%n");
+    
+    System.out.printf("flugzeug2                                                                     0    %%     0 x 130 EUR%n");
+    
+    System.out.printf("flugzeug3               x    x    x                                          25    %%     3 x 130 EUR%n");  
+    
+    System.out.printf("flugzeug4                                                                     0    %%     0 x  70 EUR%n");
+    
+    System.out.printf("flugzeug5                                                                     0    %%     0 x  70 EUR%n");
+    
+    System.out.printf("flugzeug6                                                                     0    %%     0 x  70 EUR%n");
+    
+    System.out.printf("flugzeug7                                                                     0    %%     0 x  60 EUR%n");
+    
+    System.out.printf("flugzeug8                                                                     0    %%     0 x  60 EUR%n");
+    
+    System.out.printf("total                                                                        12,96 %%        1820 EUR%n");
+    
+    System.out.println("####################################################################################################\n\n");
+    
+    System.out.println("####################################################################################################");
+  }
+}
